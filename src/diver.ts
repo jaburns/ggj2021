a3912a2f5c38aa84e9d7fad0dd705d90d8d31296
Walter <walter.gordy@gmail.com>
2021-01-30 21:41:47 -0700

    Not working. Undefined in render GameObject.render sprite.width

---
+++ src/diver.ts
@@ -0,0 +1,8 @@
+import { GameObject } from "./gameobject"
+
+export class Diver extends GameObject {
+
+    constructor() {
+        super();
+    }
+}
--- src/render.ts
+++ src/render.ts
@@ -1,46 +1,46 @@
 import { IMAGES } from "./images";
 import { GameState } from "./state";
 import { vec2 } from 'gl-matrix';
+import { Diver } from "./diver";
+import { GameObject } from "./gameobject";
 
 let ctx: CanvasRenderingContext2D;
 
-let frame: number = 0;
+let rotationframe: number = 0;
 
-function drawSprite(sprite: HTMLImageElement, x: number , y:number, scale: number = 1, rotation: number = 0) {
-    let cx = sprite.width/2;
-    let cy = sprite.height/2;
-    let cos = Math.cos(rotation), sin = Math.sin(rotation);
+let objects: any[] = []; // holds game objects to be rendered
 
-    ctx.setTransform(
-        scale * cos,
-        scale * sin,
-        scale * -sin,
-        scale * cos,
-        x,
-        y
-    );
-
-    ctx.drawImage(sprite, -cx, -cy, sprite.width, sprite.height)
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-}
+let diver = new Diver();
+let boat = new GameObject();
 
 export const initRenderer = ( canvas: HTMLCanvasElement ): void =>
 {
     ctx = canvas.getContext('2d')!;
+
+    diver.sprite = IMAGES['DiverSprite1.png'];
+    diver.scale = 0.20;
+
+    boat.sprite = IMAGES['Boat.png'];
+    boat.scale = 0.25;
+
+    objects.push(diver);
+    objects.push(boat);
 };
 
 export const render = ( state0: GameState, state1: GameState, lerpTime: number ): void =>
 {
-    let boat = IMAGES['Boat.png']
-    let sprite = IMAGES['sprite.png']
-    let diver = IMAGES['DiverSprite1.png'] // figure out sprite animation later
-
+    rotationframe += 0.01;
     ctx.clearRect( 0, 0, 1280, 720 );
 
     const lerpPlayerPos = vec2.lerp( vec2.create(), state0.playerPos, state1.playerPos, lerpTime );
 
-    drawSprite(sprite, lerpPlayerPos[0], lerpPlayerPos[1], 1, frame);
-    drawSprite(boat, 640, 0, 0.25, frame);
-    drawSprite(diver, 120, 0, 0.25, frame);
-    frame += 0.01;
+    diver.rotation =  rotationframe;
+    diver.position = lerpPlayerPos
+
+
+    // render the objects
+    objects.map((o) => {
+        o.render(ctx);
+    });
+
 };
